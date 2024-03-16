b99d8f1b7c0de760b0f7a2f93ea4ab91a9d19aed
prinln <mu-silva@outlook.com>
2024-03-16 16:02:24 -0300

    funcao init

--- 5 - Funcoes/funcao_init.go
+++ 5 - Funcoes/funcao_init.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
+// Executa primeiro por ser init inicialização inicia antes do main()
+func init() {
+	fmt.Print("teste init")
+}
+
 // var numeroMagico = 5
 func main() {
-	fmt.Print(OperacaoNomeada(1, 2))
+	fmt.Print(OperacaoNomeadaInit(1, 2))
 }
 
 // Cuidado com os escopos o Golang permiti usar variaveis dentro de funções que sobrescrever o valor no arquivo por exemplo no comentario
-func OperacaoNomeada(numero1 int, numero2 int) (soma int, multiplicao int, divisao int, subtracao int) {
+func OperacaoNomeadaInit(numero1 int, numero2 int) (soma int, multiplicao int, divisao int, subtracao int) {
 	//numeroMagico := 3
 	soma = numero1 + numero2
 	multiplicao = numero1 * numero2
